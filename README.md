6d433cb9636caf5e357443148229d6cd53782b18
Thiago Araujo <thiago_liveira@hotmail.com>
2021-03-13 16:16:42 -0300

    Updating the E2E tests for google search and youtube search

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This project is based on the lessons of Academind:
 
-https://www.youtube.com/watch?v=r9HdJ8P6GQI
+[JavaScript Testing Introduction Tutorial - Unit Tests, Integration Tests & e2e Tests](https://www.youtube.com/watch?v=r9HdJ8P6GQI)
 
 To run these tests, simply run:
 
@@ -9,3 +9,16 @@ To run these tests, simply run:
 And you can watch the tests too:
 
 `npm run test-watch`
+
+The following tests are executed on this packet:
+
+- Unit tests:
+
+  - Output name and age;
+  - Output data-less text with dummy entry;
+  - Output data-less text with empty entry.
+
+- e2e Tests:
+
+  - Google simple search sample;
+  - Youtube search with screeshot evidence.
--- util.spec.js
+++ util.spec.js
@@ -1,6 +1,10 @@
-//Unit test for util functions
+const puppeteer = require('puppeteer');
 const { generateText } = require('./util');
 
+//The visual test use to take long, so have to change the timeout of jest
+jest.setTimeout(30000);
+
+//Unit test for util functions
 //Testing partitions
 test('should output name and age', () => {
   const text = generateText('Thiago', 33);
@@ -19,4 +23,43 @@ test('should output data-less text', () => {
 test('should output data-less text', () => {
   const text = generateText();
   expect(text).toBe('undefined (undefined years old)');
+});
+
+//e2e Tests
+test('should open google', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=1920,1080'],
+  });
+  const page = await browser.newPage();
+  await page.goto('http://www.google.com');
+  await page.click('input[name="q"]');
+  await page.type('input[name="q"]', 'test', { delay: 100 });
+  //await page.type('#q', 'test', { delay: 100 });
+
+  await page.close();
+});
+
+//e2e test with evidence
+test('should make an youtube search and print the page', async () => {
+  const screenshot = 'youtube.png';
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 20,
+    args: ['--window-size=1920,1080'],
+  });
+  //const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://youtube.com');
+  await page.type('#search', 'Fleetwood Mac Dreams');
+  await page.click('button#search-icon-legacy');
+  await page.waitForSelector('ytd-thumbnail.ytd-video-renderer');
+  await page.screenshot({ path: 'youtube_fm_dreams_list.png' });
+  const videos = await page.$$('ytd-thumbnail.ytd-video-renderer');
+  await videos[2].click();
+  await page.waitForSelector('.html5-video-container');
+  await page.screenshot({ path: screenshot });
+  await browser.close();
+  console.log('See screenshot: ' + screenshot);
 });
